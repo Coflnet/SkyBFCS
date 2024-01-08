340ef062041825dedf37d2f2df2e75546ae1afd1
Äkwav <ekwav@coflnet.com>
2024-01-08 20:24:24 +0100

    simplify namespace

--- Services/SingleBazaarUpdater.cs
+++ Services/SingleBazaarUpdater.cs
@@ -2,22 +2,21 @@ using System.Threading.Tasks;
 using Coflnet.Sky.Sniper.Services;
 using Coflnet.Sky.Updater;
 
-namespace Coflnet.Sky.BFCS.Services
+namespace Coflnet.Sky.BFCS.Services;
+public class SingleBazaarUpdater : BazaarUpdater
 {
-    public class SingleBazaarUpdater : BazaarUpdater
+    private SniperService sniper;
+
+    public SingleBazaarUpdater(SniperService sniper) : base(null)
     {
-        private SniperService sniper;
+        this.sniper = sniper;
+        SecondsBetweenUpdates = 30;
+    }
 
-        public SingleBazaarUpdater(SniperService sniper) : base(null)
-        {
-            this.sniper = sniper;
-            SecondsBetweenUpdates = 30;
-        }
-
-        protected override Task ProduceIntoQueue(dev.BazaarPull pull)
-        {
-            sniper.UpdateBazaar(pull);
-            return Task.CompletedTask;
-        }
+    protected override Task ProduceIntoQueue(dev.BazaarPull pull)
+    {
+        sniper.UpdateBazaar(pull);
+        return Task.CompletedTask;
     }
 }
+
