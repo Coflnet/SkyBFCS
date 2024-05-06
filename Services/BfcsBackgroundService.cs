907826114980615c85aa4a9904aae6b23b7dcf2a
Äkwav <ekwav@coflnet.com>
2024-05-06 18:03:26 +0200

    use primary constructor

--- Services/BfcsBackgroundService.cs
+++ Services/BfcsBackgroundService.cs
@@ -4,19 +4,16 @@ using Coflnet.Sky.ModCommands.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Coflnet.Sky.Commands.Shared;
 using System;
 
-public class BfcsBackgroundService : ModBackgroundService
-{
-    public BfcsBackgroundService(
-        IServiceScopeFactory scopeFactory,
-        IConfiguration config,
-        ILogger<ModBackgroundService> logger)
-        : base(scopeFactory, config, logger, null, null)
-    {
-    }
+namespace Coflnet.Sky.BFCS.Services;
 
+public class BfcsBackgroundService(
+    IServiceScopeFactory scopeFactory,
+    IConfiguration config,
+    ILogger<ModBackgroundService> logger) 
+    : ModBackgroundService(scopeFactory, config, logger, null, null)
+{
     public event Action<LowPricedAuction> FoundSnipe;
 
     protected override Task DistributeFlipOnServer(LowPricedAuction flip)
