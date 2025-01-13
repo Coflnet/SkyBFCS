50ce9f715100ca728359d0beb6534fbcc25088fa
Äkwav <ekwav@coflnet.com>
2025-01-13 12:42:39 +0100

    disable second pass

--- Services/SnipeUpdater.cs
+++ Services/SnipeUpdater.cs
@@ -60,9 +60,8 @@ public class SnipeUpdater : NewUpdater
                     a.Context["upage"] = next.pageId.ToString();
                     a.Context["utry"] = next.tryCount.ToString();
                     a.Context["ucount"] = next.offset.ToString();
-                    sniper.TestNewAuction(a, true, true);
+                    sniper.TestNewAuction(a, true);
                     _ = Task.Run(() => NewAuction?.Invoke(a));
-                    await secondPass.Writer.WriteAsync(a).ConfigureAwait(false);
                 }
                 catch (Exception e)
                 {
